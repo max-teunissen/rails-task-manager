fc7a1106653b973c49002b524bd4bb736d7d8425
Max Teunissen <max.teunissen@mac.com>
2019-05-14 15:33:09 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root to: 'tasks#home'
   get 'tasks', to: 'tasks#index', as: :tasks
 
+  get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
 
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update', as: :update
+
   post 'tasks', to: 'tasks#create', as: :create
+  delete 'tasks/:id', to: 'tasks#destroy', as: :delete_task
 end
