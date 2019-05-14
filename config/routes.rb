95c3e097a8b85a2070ab0ec4f8fa672095473a6d
Max Teunissen <max.teunissen@mac.com>
2019-05-14 12:21:50 +0200

    Commiting the first part of the exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#home'
+  get 'tasks', to: 'tasks#index', as: :tasks
+
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  post 'tasks', to: 'tasks#create', as: :create
 end
